5d59438a8f9807ef37c28a3af89fabeaa1f62463
NurTheDev <nur756.islam@gmail.com>
2025-02-08 23:56:49 +0600

    added singup page image

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,13 +3,21 @@ import SignUpimg from "../component/SignUpImg";
 const SignUp = () => {
   //   console.log(signUpimg);
   return (
-    <div className="flex">
-      <div>
-        <h1>Get started with easily register</h1>
-        <p>Free register and you can enjoy it</p>
-      </div>
-      <div>
-        <SignUpimg />
+    <div className="container">
+      <div className="grid h-screen grid-cols-12 items-center justify-center ">
+        <div className="col-span-7 flexCol">
+          <div>
+            <h1 className="text-4xl font-nunito text-primary-purple font-bold ">
+              Get started with easily register
+            </h1>
+            <p className="font-nunito text-black-50 text-xl mt-3">
+              Free register and you can enjoy it
+            </p>
+          </div>
+        </div>
+        <div className="col-span-5">
+          <SignUpimg />
+        </div>
       </div>
     </div>
   );
