5c4e76f519b24756ea749972e26d7359c59fc9a8
NurTheDev <nur756.islam@gmail.com>
2025-02-10 00:39:03 +0600

    Update ESLint configuration and add Registration.json asset

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,22 +2,45 @@ import SignUpImg from "../component/SignUpImg";
 import Input from "../component/Input";
 import { inputSource } from "../../lib/inputSource.js";
 import { useState } from "react";
+import { PiEyeClosedDuotone } from "react-icons/pi";
 
 const SignUp = () => {
-  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  const [conPass, setConfPass] = useState("");
+  const [showPass, setShowPass] = useState(false);
   const input = inputSource();
+  // console.log(conPass);
 
   /**
-   * handleInput function;
+   * todo: handleInput function;
    * @param {event} event - The input event
    * @return null
    * The handleInput function in JavaScript React sets the value based on the input event.
    */
   const handleInput = (event) => {
-    setValue(event.target.value);
-    console.log(event.target.value);
+    const { name, value } = event.target;
+    if (name == "Username") {
+      setName(value);
+    } else if (name == "Email") {
+      setEmail(value);
+    } else if (name == "Password") {
+      setPass(value);
+    } else if (name === "Confirm Password") {
+      setConfPass(value);
+    }
   };
 
+  /**
+   * todo: ShowPass Function;
+   * @param ({})
+   * @return null
+   */
+  const getPassShow = () => {
+    setShowPass(!showPass);
+    // console.log(showPass);
+  };
   return (
     <div className="container">
       <div className="grid h-screen grid-cols-12 items-center justify-center">
@@ -33,12 +56,23 @@ const SignUp = () => {
           <div className="w-1/2">
             {input?.map((data) => (
               <div key={data.id} className="mt-14">
-                <Input
-                  name={data.name}
-                  type={data.type}
-                  className="w-full border-dark-blue px-14 border-t-2 focus:outline-0 py-7 focus:border-dark-blue"
-                  onChange={handleInput}
-                />
+                {data.type !== "password" ? (
+                  <Input
+                    name={data.name}
+                    type={data.type}
+                    className="w-full border-dark-blue px-14 border-t-2 focus:outline-0 py-7 focus:border-dark-blue"
+                    onChange={handleInput}
+                  />
+                ) : (
+                  <Input
+                    name={data.name}
+                    type={!showPass ? data.type : "text"}
+                    className="w-full border-dark-blue px-14 border-t-2 focus:outline-0 py-7 focus:border-dark-blue"
+                    onChange={handleInput}
+                  >
+                    <PiEyeClosedDuotone onClick={getPassShow} />
+                  </Input>
+                )}
               </div>
             ))}
           </div>
--- src/Pages/component/Input.jsx
+++ src/Pages/component/Input.jsx
@@ -1,9 +1,18 @@
 import { Input } from "@material-tailwind/react";
 import PropTypes from "prop-types";
+// import { children } from "react";
 
-export default function InputDefault({ name, type, className, id, onChange }) {
+export default function InputDefault({
+  name,
+  type,
+  className,
+  id,
+  onChange,
+  children,
+  onClick,
+}) {
   return (
-    <div className="mt-10 w-72">
+    <div className="mt-10 w-72 relative">
       <Input
         label={name}
         name={name}
@@ -12,6 +21,12 @@ export default function InputDefault({ name, type, className, id, onChange }) {
         id={id}
         onChange={onChange}
       />
+      <div
+        className="absolute right-4 top-[50%] text-xl cursor-pointer"
+        onClick={onClick}
+      >
+        {children}
+      </div>
     </div>
   );
 }
@@ -22,4 +37,6 @@ InputDefault.propTypes = {
   className: PropTypes.string,
   id: PropTypes.string,
   onChange: PropTypes.func,
+  children: PropTypes.string,
+  onClick: PropTypes.func,
 };
